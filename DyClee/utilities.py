56974258e7da93e04845c207b32792c3fd1c6d5f
Daniel Mallia <Daniel.Mallia21@myhunter.cuny.edu>
2020-11-15 14:39:20 -0500

    Add utilities module with function names, incomplete signatures

---
+++ DyClee/utilities.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+# Decay functions
+
+def linear_decay(t, tlk):
+	pass
+	
+
+
+def trapezoidal_decay(t, tlk):
+	pass
+
+
+def zshape_decay(t, tlk):
+	pass
+
+
+def exponential_decay(t, tlk):
+	pass
+
+
+def halflife_decay(t, tlk):
+	pass
+
+
+def sigmoidal_decay(t, tlk):
+	pass
+
+
+# Distance
+
+def manhattan_distance(a, b):
+	return np.sum(np.abs(a - b))
+
