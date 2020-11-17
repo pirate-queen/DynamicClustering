a4ee97a6ecaa38d4a1efc7b7f4579d3324067542
Daniel Mallia <Daniel.Mallia21@myhunter.cuny.edu>
2020-11-16 19:18:31 -0500

    Add linear_decay
    Co-authored-by: A.S.Leshchenko <20774501+piratevector@users.noreply.github.com>

--- DyClee/utilities.py
+++ DyClee/utilities.py
@@ -1,11 +1,25 @@
 import numpy as np
 
+# Citation:
+# Nathalie Barbosa Roa, Louise Travé-Massuyès, Victor Hugo Grisales.
+# DyClee: Dynamic clustering for tracking evolving environments.
+# Pattern Recognition, Elsevier, 2019, 94, pp.162-186.
+# 10.1016/j.patcog.2019.05.024 . hal-02135580
+
 # Decay functions
+# All functions take metaparameters upon initialization, but expect
+# current time (t) and last time (tlk) at runtime
 
-def linear_decay(t, tlk):
-	pass
-	
-
+# Implements linear decay (page 12)
+# tw_zero = time func takes to go from 1 to 0
+# m = function slope
+def linear_decay(tw_zero, m):
+	def ld(t, tlk):
+		if t - tlk > tw_zero:
+			return 0
+		else:
+			return 1-m*(t-tlk)
+	return ld
 
 def trapezoidal_decay(t, tlk):
 	pass
